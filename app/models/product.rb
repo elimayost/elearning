5082a21916d32582b2fc12f06acea5885599666e
Eli Mayost <elimayost@elimayost-laptop.(none)>
2010-08-02 22:33:18 +0100

    Initial commit

--- app/models/product.rb
+++ app/models/product.rb
@@ -5,6 +5,6 @@ class Product < ActiveRecord::Base
 	validates :name, :presence => true, :uniqueness => true
 	validates :description, :presence => true
 	validates :price, :presence => true, :numericality => true, :not_zero => true
-	validates :download_url, :presence => true, :uniqueness => true
+	validates :download_url, :presence => true, :uniqueness => true, :url_format => true
 
 end
