808feab355f89b8b60287054f95e20eaeaa25ccb
Eli Mayost <elimayost@elimayost.(none)>
2010-08-03 16:57:59 +0100

    Added login jquery

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,2 +1,14 @@
-// Place your application-specific JavaScript functions and classes here
-// This file is automatically included by javascript_include_tag :defaults
+
+jQuery(function() {
+
+	jQuery('.login').click(function() {
+		jQuery('login').dialog({
+			autoOpen: true,
+			height: 200,
+			width: 300,
+			modal: true,
+		});
+	});
+
+});
+
