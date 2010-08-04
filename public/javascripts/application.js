a79b45354141eb6b6316beda8e2f6a0be6a0b54f
Eli Mayost <elimayost@elimayost.(none)>
2010-08-04 16:43:35 +0100

    Added jQuery login box

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,13 +1,26 @@
 
 jQuery(function() {
 
-	jQuery('.login').click(function() {
-		jQuery('login').dialog({
+	jQuery('#login-button').click(function() {
+
+        jQuery('input#email').val('');
+        jQuery('input#password').val('');
+
+        login_button_position = jQuery('#login-button').position();
+
+		jQuery('#login').dialog({
+			title: 'Login details: (press Esc key to close)',
+			resizable: false,
+			position: [login_button_position.left-10, login_button_position.top+28],
 			autoOpen: true,
-			height: 200,
-			width: 300,
+			show: 'blind',
+			height: 130,
+			width: 280,
 			modal: true,
+			draggable: false,
+			open: function(event, ui) { jQuery(".ui-dialog-titlebar-close").hide(); }
 		});
+
 	});
 
 });
